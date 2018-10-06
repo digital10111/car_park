dcffa3156f96fec16b8c36e54ee16292e4ac47f0
digital10111 <karan.verma@flipkart.com>
2018-10-06 14:10:47 +0530

    removed slot in car

--- entities/Car.py
+++ entities/Car.py
@@ -2,4 +2,3 @@ class Car:
     def __init__(self, color, registration_number):
         self.color = color
         self.registration_number = registration_number
-        self.parking_slot_number = -1
